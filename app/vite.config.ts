91a1dcf0f214410e1238cb86a6cb3ef6aa34a38c
Sebastian Werner <s.werner@sebastian-software.de>
2022-08-16 16:27:35 +0200

    Minor changes

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -2,7 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react({
-    jsxImportSource: "@emotion/react"
-  })]
+  optimizeDeps: {
+    // Tweak settings to work-around issue: https://github.com/vitejs/vite/issues/9705
+    // include: ['hoist-non-react-statics'],
+    // exclude: ['@emotion/react']
+  },
+  plugins: [
+    react({
+      jsxImportSource: "@emotion/react"
+    })
+  ]
 })
